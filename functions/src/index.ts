8d8cb701fc4ca435eb84d87c4d3e7fb37521d137
Clive Chan <cc@clive.io>
2018-07-04 06:20:23 -0400

    basically works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 /*
 
+Note: npm i -g firebase-tools
+
 Basically my task is to do this:
 https://stackoverflow.com/questions/48620140/use-the-google-navigation-card-in-my-dialogflow-agent
 https://dialogflow.com/docs/fulfillment
@@ -23,7 +25,8 @@ const functions = require('firebase-functions');
 const {Permission, dialogflow, Suggestions, BasicCard} = require('actions-on-google');
 
 const gMaps = require('@google/maps').createClient({
-  key: process.env.GMAPS_API_KEY
+  key: process.env.GMAPS_API_KEY,
+  Promise: Promise
 });
 
 const app = dialogflow({debug: true});
@@ -38,37 +41,42 @@ app.intent('Location', agent => {
     context: `To find the nearest Loblaw's location to you`,
     permissions: ['DEVICE_PRECISE_LOCATION'],
   }));
+  agent.ask(new Suggestions([`Yes`, `No`]));
 });
 
 app.intent('LocationGranted', (agent, params, granted) => {
   if (granted && agent.device.location) {
-    const locationbias = `point:${agent.device.location.coordinates.latitude},${agent.device.location.coordinates.longitude}`;
-    console.log(locationbias);
-    agent.ask(`You are at ${locationbias}`);
-    gMaps.findPlace({
+    const coords = `${agent.device.location.coordinates.latitude},${agent.device.location.coordinates.longitude}`;
+    return gMaps.findPlace({
       input: 'loblaws',
       inputtype: 'textquery',
-      locationbias: locationbias,
+      locationbias: 'point:' + coords,
       fields: 'place_id',
-    }).asPromise().then(res => {
-      agent.ask(JSON.stringify(res));
+    }).asPromise()
+    .then(res => {
+      // Choose if res.json.candidates.length > 1
+      return gMaps.directions({
+        origin: coords,
+        destination: 'loblaws' // 'place_id:' + res.json.candidates[0].place_id
+      }).asPromise();
+    })
+    .then(res => {
+      agent.ask(`Summary of directions to Loblaws: ` + JSON.stringify(res.json.routes[0].summary));
+      agent.ask(new BasicCard({
+        title: `Directions to Loblaws`,
+        //subtitle: `It should take about 10 minutes.`,
+        //image: {
+        //  url: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+        //  accessibilityText: 'Directions to Loblaws',
+        //},
+        buttons: [{
+          title: 'View on Google Maps',
+          openUrlAction: {url: 'https://www.google.ca/maps/dir/?api=1&origin=@'+coords+'&destination=Loblaws'},
+        }],
+      }));
     }).catch(err => {
       agent.ask("An error occurred: " + JSON.stringify(err));
     });
-    agent.ask(`Here are directions to Loblaws:`);
-    agent.ask(new BasicCard({
-      title: `Directions to Loblaws`,
-      subtitle: `It should take about 10 minutes.`,
-      image: {
-        url: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-        accessibilityText: 'Directions to Loblaws',
-      },
-      buttons: [{
-        title: 'View on Google Maps',
-        openUrlAction: {url: 'https://assistant.google.com/'},
-      }],
-    }));
-    agent.ask(new Suggestions([`Yes`, `No`]));
     // agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
   } else {
     agent.ask('Sorry, I could not figure out where you are.');
