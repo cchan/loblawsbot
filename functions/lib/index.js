b603a0fd16d0ad88522887c9babd2a64b4dcbff2
Clive Chan <cc@clive.io>
2018-07-04 05:15:05 -0400

    almost works - now it's a local project with firebase deploy

---
+++ functions/lib/index.js
@@ -0,0 +1,75 @@
+/*
+
+Basically my task is to do this:
+https://stackoverflow.com/questions/48620140/use-the-google-navigation-card-in-my-dialogflow-agent
+https://dialogflow.com/docs/fulfillment
+https://developers.google.com/maps/documentation/javascript/directions
+
+https://medium.com/google-developer-experts/handling-permissions-with-dialogflow-and-actions-on-google-b08c8f228c00
+sample code is good
+
+https://discuss.api.ai/t/google-assistant-rich-message-responses/5134/19
+
+https://developers.google.com/places/web-service/search#FindPlaceRequests
+
+https://firebase.google.com/docs/functions/get-started
+*/
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv-safe').config();
+const functions = require('firebase-functions');
+const { Permission, dialogflow, Suggestions, BasicCard } = require('actions-on-google');
+const gMaps = require('@google/maps').createClient({
+    key: process.env.GMAPS_API_KEY
+});
+const app = dialogflow({ debug: true });
+app.intent('Default Welcome Intent', agent => {
+    agent.ask(`Hello! I'm your virtual Loblaws assistant. Ask me anything - for example, where's the nearest Loblaws?`);
+    agent.ask(new Suggestions([`Where's the nearest Loblaws?`, `Directions to Loblaws`]));
+});
+app.intent('Location', agent => {
+    agent.ask(new Permission({
+        context: `To find the nearest Loblaw's location to you`,
+        permissions: ['DEVICE_PRECISE_LOCATION'],
+    }));
+});
+app.intent('LocationGranted', (agent, params, granted) => {
+    if (granted && agent.device.location) {
+        const locationbias = `point:${agent.device.location.coordinates.latitude},${agent.device.location.coordinates.longitude}`;
+        console.log(locationbias);
+        agent.ask(`You are at ${locationbias}`);
+        gMaps.findPlace({
+            input: 'loblaws',
+            inputtype: 'textquery',
+            locationbias: locationbias,
+            fields: 'place_id',
+        }).asPromise().then(res => {
+            agent.ask(JSON.stringify(res));
+        }).catch(err => {
+            agent.ask("An error occurred: " + JSON.stringify(err));
+        });
+        agent.ask(`Here are directions to Loblaws:`);
+        agent.ask(new BasicCard({
+            title: `Directions to Loblaws`,
+            subtitle: `It should take about 10 minutes.`,
+            image: {
+                url: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+                accessibilityText: 'Directions to Loblaws',
+            },
+            buttons: [{
+                    title: 'View on Google Maps',
+                    openUrlAction: { url: 'https://assistant.google.com/' },
+                }],
+        }));
+        agent.ask(new Suggestions([`Yes`, `No`]));
+        // agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    }
+    else {
+        agent.ask('Sorry, I could not figure out where you are.');
+    }
+});
+app.intent('InStock', agent => {
+    agent.ask(`Got an in stock request ${JSON.stringify(agent.parameters)}`);
+});
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+//# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,13 +8,23 @@ https://developers.google.com/maps/documentation/javascript/directions
 https://medium.com/google-developer-experts/handling-permissions-with-dialogflow-and-actions-on-google-b08c8f228c00
 sample code is good
 
+https://discuss.api.ai/t/google-assistant-rich-message-responses/5134/19
+
+https://developers.google.com/places/web-service/search#FindPlaceRequests
+
+https://firebase.google.com/docs/functions/get-started
 */
 
 
 'use strict';
 
+require('dotenv-safe').config();
 const functions = require('firebase-functions');
 const {Permission, dialogflow, Suggestions, BasicCard} = require('actions-on-google');
+
+const gMaps = require('@google/maps').createClient({
+  key: process.env.GMAPS_API_KEY
+});
 
 const app = dialogflow({debug: true});
 
@@ -32,8 +42,19 @@ app.intent('Location', agent => {
 
 app.intent('LocationGranted', (agent, params, granted) => {
   if (granted && agent.device.location) {
-    const location = agent.device.location;
-    agent.ask(`You are at ${JSON.stringify(location)}`);
+    const locationbias = `point:${agent.device.location.coordinates.latitude},${agent.device.location.coordinates.longitude}`;
+    console.log(locationbias);
+    agent.ask(`You are at ${locationbias}`);
+    gMaps.findPlace({
+      input: 'loblaws',
+      inputtype: 'textquery',
+      locationbias: locationbias,
+      fields: 'place_id',
+    }).asPromise().then(res => {
+      agent.ask(JSON.stringify(res));
+    }).catch(err => {
+      agent.ask("An error occurred: " + JSON.stringify(err));
+    });
     agent.ask(`Here are directions to Loblaws:`);
     agent.ask(new BasicCard({
       title: `Directions to Loblaws`,
@@ -47,6 +68,7 @@ app.intent('LocationGranted', (agent, params, granted) => {
         openUrlAction: {url: 'https://assistant.google.com/'},
       }],
     }));
+    agent.ask(new Suggestions([`Yes`, `No`]));
     // agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
   } else {
     agent.ask('Sorry, I could not figure out where you are.');
@@ -57,4 +79,4 @@ app.intent('InStock', agent => {
   agent.ask(`Got an in stock request ${JSON.stringify(agent.parameters)}`);
 });
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+export const dialogflowFirebaseFulfillment = functions.https.onRequest(app);
